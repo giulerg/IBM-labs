0a08843449179ce4caad8aaafaa39125df0da7b1
giulerg <henrytheanxious@gmail.com>
2024-10-02 18:21:13 +0300

    labs

--- lab_1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-path='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/automobileEDA.csv'
-
-df = pd.read_csv(path)
-print(df.head())
-print(df['peak-rpm'].dtypes)
